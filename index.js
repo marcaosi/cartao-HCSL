232568df0aa347ce556ceea8f99a65c3e126dc35
Francisco R. M. Porfírio <80773101+f-rxfxel@users.noreply.github.com>
2023-10-04 12:34:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 const Jimp = require('jimp');
 
-const nome = "Pedro Henrique Manuel Lopes"
+const cor = "verde"
+let nome = "Pedro Henrique Manuel Lopes".toUpperCase()
 const cpf = "174.361.092-00"
 const id = "09345-001"
 
 async function textOverlay() {
-    // Reading image
-    const image = await Jimp.read('./cartoes/1.jpg');
-    // Defining the text font
-    const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
-    image.print(font, 107, 580, nome, 820);
-    image.print(font, 107, 949, cpf, 820);
-    image.print(font, 107, 1227, id, 820);
-    // Writing image after processing
-    await image.writeAsync('./cartoes/output/test.jpg');
+  const image = await Jimp.read(`./cartoes/${cor}.jpg`);
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+  image.print(font, 107, 580, nome, 820);
+  image.print(font, 107, 949, cpf, 820);
+  image.print(font, 107, 1227, id, 820);
+  await image.writeAsync(`./cartoes/saida/cartao-${id}.jpg`);
 }
 
 textOverlay();
 console.log("Cartão gerado!");
 
-var QRCode = require('qrcode')
-const fs = require("fs");
+const qrcode = require('qrcode');
+const dados = id;
+const tamanho = 895;
 
-QRCode.toDataURL(id, function (err, url) {
-    console.log(url)
-    var urlCorreta = url.replace("data:image/png;base64,", "")
-    const buffer = Buffer.from(urlCorreta, "base64");
-    fs.writeFileSync("qr-code.png", buffer);
-})
+qrcode.toFile(`./cartoes/saida/qr-${id}.png`, dados, {
+  width: tamanho,
+  height: tamanho,
+}, function (err) {
+  if (err) throw err;
+  console.log('QR Code gerado!');
+});
+
+
+
+
+// teste de overlay de imagens
+
+
+Jimp.read(`./cartoes/saida/cartao-${id}.jpg`, (err, fir_img) => {
+  if(err) {
+      console.log(err);
+  } else {
+      Jimp.read(`./cartoes/saida/qr-${id}.png`, (err, sec_img) => {
+          if(err) {
+              console.log(err);
+          } else {
+              fir_img.blit(sec_img, 1258, 362);
+              fir_img.write(`${id}.png`);
+          }
+      })
+  }
+});
