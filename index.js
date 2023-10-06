fdbee2541e00ebcde82961eeb33e80c74ceff0bc
Marcos Antonio Dos Santos <marcosantonio@fuvs.br>
2023-10-06 09:10:05 -0300

    refatorando para api

--- index.js
+++ index.js
@@ -2,64 +2,88 @@ const Jimp = require('jimp')
 const qrcode = require('qrcode')
 const fs = require('fs').promises
 
-const cor = "verde"
-const nome = "Pedro Henrique Manuel Lopes".toUpperCase()
-const cpf = "174.361.092-00"
-const id = "09345-001"
 
-async function textOverlay() {
+
+async function textOverlay(nome, cpf, tipo, id) {
   try {
-    const image = await Jimp.read(`./cartoes/${cor}.jpg`)
+    const image = await Jimp.read(`./cartoes/${tipo}.jpg`)
     const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE)
+    const filename = `./cartoes/saida/cartao-${id}.jpg`
+
     image.print(font, 107, 580, nome, 820)
     image.print(font, 107, 949, cpf, 820)
     image.print(font, 107, 1227, id, 820)
-    console.log("Cartão gerado!")
-    await image.writeAsync(`./cartoes/saida/cartao-${id}.jpg`)
+
+    await image.writeAsync(filename)
+
+    return filename
   } catch (error) {
-    console.error("Erro ao processar imagem:", error)
+    throw new Error(`Falha ao gerar a imagem base: nome: ${nome}, tipo: ${tipo}`)
   }
 }
 
-async function generateQRCode() {
+async function generateQRCode(id) {
   try {
     const tamanho = 895
-    await qrcode.toFile(`./cartoes/saida/qr-${id}.png`, id, {
+    const filename = `./cartoes/saida/qr-${id}.png`
+
+    qrcode.toFile(filename, id, {
       width: tamanho,
       height: tamanho,
     })
-    console.log('QR Code gerado!')
+    
+    return filename
   } catch (error) {
-    console.error("Erro ao gerar QR Code:", error)
+    throw new Error(`Erro ao gerar QR Code: ${id}`)
   }
 }
 
-async function mergeImages() {
+async function mergeImages(baseImage, qrcodeImage, id) {
   try {
-    const fir_img = await Jimp.read(`./cartoes/saida/cartao-${id}.jpg`)
-    const sec_img = await Jimp.read(`./cartoes/saida/qr-${id}.png`)
+    const fir_img = await Jimp.read(baseImage)
+    const sec_img = await Jimp.read(qrcodeImage)
+    const filename = `./cartoes/saida/${id}.png`
+
     fir_img.blit(sec_img, 1258, 362)
-    await fir_img.writeAsync(`./cartoes/saida/${id}.png`)
-    console.log(`${id}.png gerado`)
+    
+    await fir_img.writeAsync(filename)
+
+    return filename
   } catch (error) {
-    console.error("Erro ao mesclar imagens:", error)
+    throw new Error(`Erro ao mesclar imagens: ${baseImage}, ${qrcodeImage} => ${id}`)
   }
 }
 
-async function cleanUp() {
+async function cleanUp(filename) {
   try {
-    await fs.unlink(`./cartoes/saida/qr-${id}.png`)
-    await fs.unlink(`./cartoes/saida/cartao-${id}.jpg`)
+    await fs.unlink(filename)
   } catch (error) {
-    console.error("Erro ao excluir arquivos:", error)
+    console.error(`Erro ao excluir arquivo: ${filename}`)
   }
 }
 
 async function main() {
-  await textOverlay()
-  await generateQRCode()
-  await mergeImages()
-  await cleanUp()
+
+  const tipo = 0
+  const nome = "Pedro Henrique Manuel Lopes".toUpperCase()
+  const cpf = "174.361.092-00"
+  const id = "09345-001"
+
+  let cor = "verde"
+
+  if(tipo == 1){
+    cor = "verde-agua"
+  }else if(tipo == 2){
+    cor = "azul"
+  }
+
+  // const { cor, nome, cpf, id} = req.body
+
+  const baseImage = await textOverlay(nome, cpf, cor, id)
+  const qrcodeImage = await generateQRCode(id)
+  const cartao = await mergeImages(baseImage, qrcodeImage, id)
+  await cleanUp(baseImage)
+  await cleanUp(qrcodeImage)
 }
 
 main()
