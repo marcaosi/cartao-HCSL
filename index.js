b5b02490cb91ab4682fe6b54760de6299dcdc4ae
Francisco Rafael Martins Porfírio <80773101+f-rxfxel@users.noreply.github.com>
2023-10-06 08:22:19 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,51 +1,65 @@
-const Jimp = require('jimp');
+const Jimp = require('jimp')
+const qrcode = require('qrcode')
+const fs = require('fs').promises
 
 const cor = "verde"
-let nome = "Pedro Henrique Manuel Lopes".toUpperCase()
+const nome = "Pedro Henrique Manuel Lopes".toUpperCase()
 const cpf = "174.361.092-00"
 const id = "09345-001"
 
 async function textOverlay() {
-  const image = await Jimp.read(`./cartoes/${cor}.jpg`);
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
-  image.print(font, 107, 580, nome, 820);
-  image.print(font, 107, 949, cpf, 820);
-  image.print(font, 107, 1227, id, 820);
-  await image.writeAsync(`./cartoes/saida/cartao-${id}.jpg`);
+  try {
+    const image = await Jimp.read(`./cartoes/${cor}.jpg`)
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE)
+    image.print(font, 107, 580, nome, 820)
+    image.print(font, 107, 949, cpf, 820)
+    image.print(font, 107, 1227, id, 820)
+    console.log("Cartão gerado!")
+    await image.writeAsync(`./cartoes/saida/cartao-${id}.jpg`)
+  } catch (error) {
+    console.error("Erro ao processar imagem:", error)
+  }
 }
 
-textOverlay();
-console.log("Cartão gerado!");
-
-const qrcode = require('qrcode');
-const dados = id;
-const tamanho = 895;
-
-qrcode.toFile(`./cartoes/saida/qr-${id}.png`, dados, {
-  width: tamanho,
-  height: tamanho,
-}, function (err) {
-  if (err) throw err;
-  console.log('QR Code gerado!');
-});
-
-
-
-
-// teste de overlay de imagens
-
-
-Jimp.read(`./cartoes/saida/cartao-${id}.jpg`, (err, fir_img) => {
-  if(err) {
-      console.log(err);
-  } else {
-      Jimp.read(`./cartoes/saida/qr-${id}.png`, (err, sec_img) => {
-          if(err) {
-              console.log(err);
-          } else {
-              fir_img.blit(sec_img, 1258, 362);
-              fir_img.write(`${id}.png`);
-          }
-      })
+async function generateQRCode() {
+  try {
+    const tamanho = 895
+    await qrcode.toFile(`./cartoes/saida/qr-${id}.png`, id, {
+      width: tamanho,
+      height: tamanho,
+    })
+    console.log('QR Code gerado!')
+  } catch (error) {
+    console.error("Erro ao gerar QR Code:", error)
   }
-});
+}
+
+async function mergeImages() {
+  try {
+    const fir_img = await Jimp.read(`./cartoes/saida/cartao-${id}.jpg`)
+    const sec_img = await Jimp.read(`./cartoes/saida/qr-${id}.png`)
+    fir_img.blit(sec_img, 1258, 362)
+    await fir_img.writeAsync(`./cartoes/saida/${id}.png`)
+    console.log(`${id}.png gerado`)
+  } catch (error) {
+    console.error("Erro ao mesclar imagens:", error)
+  }
+}
+
+async function cleanUp() {
+  try {
+    await fs.unlink(`./cartoes/saida/qr-${id}.png`)
+    await fs.unlink(`./cartoes/saida/cartao-${id}.jpg`)
+  } catch (error) {
+    console.error("Erro ao excluir arquivos:", error)
+  }
+}
+
+async function main() {
+  await textOverlay()
+  await generateQRCode()
+  await mergeImages()
+  await cleanUp()
+}
+
+main()
